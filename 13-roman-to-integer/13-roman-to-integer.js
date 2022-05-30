e402129faa228c5ef6a99c72f9fbcb4e0e6ca9b3
jmj0k <101824840+jmj0k@users.noreply.github.com>
2022-05-30 22:06:53 +0900

    Time: 200 ms (40.48%), Space: 47.2 MB (59.94%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    s = s.split('');
+    let count = 0;
+    let roman = {
+       'I': 1,
+       'V': 5,
+       'X': 10,
+       'L': 50,
+       'C': 100,
+       'D': 500,
+       'M': 1000,
+    };
+    
+    for(let i = 0; i < s.length; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            count += roman[s[i + 1]] - roman[s[i]];
+            i++;
+        } else count += roman[s[i]];
+    }
+    
+    return count;
+};
