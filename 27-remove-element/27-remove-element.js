5cc004d121d2be4b04f59724c3e0e0d3a11f26cb
jmj0k <101824840+jmj0k@users.noreply.github.com>
2022-05-31 22:27:36 +0900

    Time: 68 ms (76.03%), Space: 42 MB (72.26%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    while (nums.includes(val)) {
+        nums.splice(nums.indexOf(val), 1);
+    }
+};
