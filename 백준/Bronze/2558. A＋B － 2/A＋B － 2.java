18ea27ca09de417b82a1bd641a07b039f94b1208
JENNY <101824840+jmj0k@users.noreply.github.com>
2022-06-10 00:07:05 +0900

    [Bronze V] Title: A+B - 2, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int f1 = sc.nextInt();
+        int f2 = sc.nextInt();
+        
+        System.out.println(f1+f2);
+    }
+}
