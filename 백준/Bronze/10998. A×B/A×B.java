e687313bfc8d4602f8d4e65fc495e9c37010a62a
JENNY <101824840+jmj0k@users.noreply.github.com>
2022-06-10 00:08:23 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int fnum = scanner.nextInt();
+		int snum = scanner.nextInt();
+		int sum = fnum * snum;
+		System.out.println(sum);
+	}
+}
