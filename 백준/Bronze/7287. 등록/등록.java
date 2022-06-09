df934c8983d7a01883146fbd870e3a40b12a64c4
JENNY <101824840+jmj0k@users.noreply.github.com>
2022-06-10 00:07:44 +0900

    [Bronze V] Title: 등록, Time: 84 ms, Memory: 10768 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+         System.out.println("6");
+        System.out.println("codezero");
+        
+    }
+}
