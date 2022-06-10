3d0e3cfd7a131dce3daea6fc77139ba4b6ea778d
JENNY <101824840+jmj0k@users.noreply.github.com>
2022-06-10 22:10:28 +0900

    [Bronze III] Title: 기찍 N, Time: 1024 ms, Memory: 32328 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+        for(int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
