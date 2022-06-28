9f17856c68ef7afa02bd92d43adf5428a98f9ffc
JENNY <101824840+jmj0k@users.noreply.github.com>
2022-06-28 11:58:03 +0900

    code update
    code update

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,62 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class Main {
-    private static int[] primeNum;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        findPrimeNum(n);  // 소수 찾기
-
-        int low = 0, high = 0;
-        int sum = primeNum[0];
-        int result = 0;
-
-        // 포인터를 이동시키며 소수 배열 탐색
-        while (low <= high && high < primeNum.length - 1) {
-            if (sum < n) {  // sum이 n보다 작은 경우
-                sum += primeNum[++high];  // high를 한칸 뒤로 이동시키고 그 값을 sum에 추가
-            } else if (sum == n) {  // sum이 n과 같은 경우
-                result += 1;  // 결과를 하나 증가시키고
-                sum += primeNum[++high];  // high를 한칸 뒤로 이동시키고 그 값을 sum에 추가
-            } else if (sum > n) {  // sum이 n보다 큰 경우
-                sum -= primeNum[low++];  // low가 가리키는 값을 sum에서 빼고 low의 위치를 한칸 이동
-            }
-        }
-
-        // 구해진 소수의 마지막 수가 n과 같은 경우 result + 1 해주기
-        if (primeNum[primeNum.length - 1] == n) {
-            result += 1;
-        }
-
-        System.out.println(result);
-    }
-
-    // 주어진 수보다 작거나 같은 소수 모두 찾기
-    private static void findPrimeNum(int n) {
-        boolean[] isPrimeNum = new boolean[n + 1];
-        Arrays.fill(isPrimeNum, true);
-
-        ArrayList<Integer> primeNumList = new ArrayList<>();
-
-        for (int i = 2; i < n + 1; i++) {
-            if (isPrimeNum[i]) {  // 해당 인덱스가 소수인 경우
-                int j = 2;
-                while (i * j < n + 1) {  // 해당 인덱스의 배수를 모두 소수가 아니도록 처리
-                    isPrimeNum[i * j] = false;
-                    j += 1;
-                }
-                primeNumList.add(i);  // 해당 인덱스는 소수이므로 추가
-            }
-        }
-
-        primeNum = primeNumList.stream().mapToInt(i -> i).toArray();
-        // 소수가 없는 경우 임의로 0을 저장하여 반환
-        if (primeNum.length == 0) {
-            primeNum = new int[1];
-        }
-    }
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	boolean[] prime = new boolean[N + 1];
+    	ArrayList<Integer> primeNumList = new ArrayList<>();
+    	Arrays.fill(prime, true);
+    	for (int i = 2; i <= N; i++) {
+    		if (!prime[i]) continue;
+    		for (int j = 2; j * i <= N; j ++) {
+    			prime[j * i] = false;
+    		}
+    		primeNumList.add(i);
+    	}
+    	int[] primeNum = primeNumList.stream().mapToInt(i -> i).toArray();
+    	if (primeNum.length == 0) primeNum = new int[1];
+    	
+    	int en = 0;
+    	int count = 0;
+    	for (int st = 0; st < primeNum.length;) {
+    		int sum = 0;
+    		while (en < primeNum.length && sum < N) {
+    			sum += primeNum[en];
+    			en++;
+    		}
+    		if (sum == N) count++;
+    		if (en == N) break;
+    		st++;
+    		en = st;
+    	}
+    	System.out.println(count);
+	}
 }
