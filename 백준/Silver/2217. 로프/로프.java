4ebb8c5e064add7842fc8700b63ed6a32d8f6d5a
JENNY <101824840+jmj0k@users.noreply.github.com>
2022-06-29 22:50:27 +0900

    [Silver IV] Title: 로프, Time: 1000 ms, Memory: 108436 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+	public static int solution(int[] arr) {
+		Arrays.sort(arr);
+		int maxWeight = 0; 
+		for (int i = 0; i < arr.length; i++) {
+			maxWeight = Math.max(maxWeight, arr[i]);
+			maxWeight = Math.max(maxWeight, arr[i] * (arr.length - i));
+			
+		}
+		return maxWeight;
+	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	int[] arr = new int[n];
+    	for (int i = 0; i < n; i++) {
+    		arr[i] = sc.nextInt();
+    	}
+    	System.out.println(solution(arr));
+	}
+}
